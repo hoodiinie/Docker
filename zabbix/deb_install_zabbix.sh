a554dfe57a56f27302751e6f0e52b842bcad38e7
hoodiinie <whitehoodieni@outlook.fr>
2023-09-15 10:26:08 +0200

    Update Docker install

--- zabbix/deb_install_zabbix.sh
+++ zabbix/deb_install_zabbix.sh
@@ -10,9 +10,18 @@ function install_docker ()
 {
     tput setaf 6; echo ""
 
-    apt-get install -y apt-transport-https ca-certificates curl gnupg2 software-properties-common
-    curl -fsSL https://download.docker.com/linux/$(. /etc/os-release; echo "$ID")/gpg | apt-key add -
-    add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/$(. /etc/os-release; echo "$ID") $(lsb_release -cs) stable"
+    apt-get update
+    apt-get install ca-certificates curl gnupg
+    install -m 0755 -d /etc/apt/keyrings
+    curl -fsSL https://download.docker.com/linux/debian/gpg | gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+    chmod a+r /etc/apt/keyrings/docker.gpg
+
+    echo \
+  "deb [arch="$(dpkg --print-architecture)" signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/debian \
+  "$(. /etc/os-release && echo "$VERSION_CODENAME")" stable" | \
+  tee /etc/apt/sources.list.d/docker.list > /dev/null
+    apt-get update
+
     apt-get update
     apt-get -y install docker-ce docker-compose
     systemctl enable docker
